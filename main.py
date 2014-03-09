6a283e6a68fb1c4e693ca981b426726c0ef893be
ilya-yamschikov <ilya.yamschikov@gmail.com>
2014-03-09 21:50:00 +0400

    modify regex

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import re
 
+LINES_TO_READ = 100
+
 f = open('data/search_log_10000.txt', 'r')
 
 i = 0
@@ -9,11 +11,13 @@ for line in f:
     print('get line of ' + str(len(a)) + ' elements.')
     if len(a) > 0:
         print("    1st element is: timestamp = " + str(a[0]['ts']) + '; address = ' + a[0]['url'])
-        match = re.match(r"^http://(www\.)?([^.]*).*", a[0]['url'])
+        match = re.match(r".*(yandex|google).*\.tr.*", a[0]['url'])
         if match:
-            print('    Site is: ' + str(match.group(2)))
+            print('    in the beginning was... ' + str(match.group(1)))
+        else:
+            print('    Site isn\'t recognized.')
     else:
         print('    Nothing to show')
     i += 1
-    if i > 2:
+    if i > LINES_TO_READ:
         break
