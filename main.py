9069e7883e4a50f9031b85ba1828be8d534ceb27
ilya-yamschikov <ilya.yamschikov@gmail.com>
2014-03-13 00:22:25 +0400

    test

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ f = open('data/search_log_10000.txt', 'r')
 i = 0
 for line in f:
     a = json.loads(line)
-    print('get line of ' + str(len(a)) + ' elements.')
+    print(str(i+1)+'get line of ' + str(len(a)) + ' elements.')
     if len(a) > 0:
         print("    1st element is: timestamp = " + str(a[0]['ts']) + '; address = ' + a[0]['url'])
-        match = re.match(r".*(yandex|google).*\.tr.*", a[0]['url'])
+        match = re.match(r".*(yandex|google)(.*\.tr).*", a[0]['url'])
         if match:
-            print('    in the beginning was... ' + str(match.group(1)))
+            print('    in the beginning was... ' + str(match.group(1)) + " in domen " + str(match.group(2)))
         else:
             print('    Site isn\'t recognized.  ')
     else:
