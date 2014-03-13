e8d7375358dfd9682426589e3fddc69c7d5290eb
ilya-yamschikov <ilya.yamschikov@gmail.com>
2014-03-13 19:34:23 +0400

    Creation of res_cut right version

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ for line in f:
                     count_google[i] += 1
             j +=1
         print('Yandex: '+str(100*count_yandex[i]/len(a))+'%  Google: '+ str(100*count_google[i]/len(a))+'% All: ' + str(100*(count_yandex[i]+count_google[i])/len(a))+'%', file=log)
-        if (count_yandex[i]!=len(a) and count_google[i]!=len(a)):
+        if (count_google[i]!=len(a)):
             print(a, file=f1)
     else:
         print("    Nothing to show", file=log)
